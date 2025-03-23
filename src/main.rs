f2993e2c25f5ab188ac253bb0ab1763b40e2c8ca
mohnishsalian0 <mohnishsalian02@gmail.com>
2025-03-23 14:26:33 +0530

    Update to main file for testing multicolor efficiency. Redundant commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#![allow(
+    clippy::items_after_test_module,
+    unused_imports,
+    unused_variables,
+    dead_code,
+    mixed_script_confusables
+)]
+
 use std::error::Error;
 use std::path::Path;
 
@@ -6,18 +14,19 @@ use qr_pro_max::{ECLevel, Palette, Version};
 use qr_pro_max::{MaskPattern, QRBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let data = "Hello, world! HeHello, world! HeHello, world!";
+    let data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam rhoncus tempor erat, nec luctus leo pharetra a. Ut dignissim lacus ut iaculis vehicula. Phasellus nec consequat est, vel lobortis nisl. Proin id imperdiet eros, vitae pharetra odio. Curabitur blandit id ipsum a efficitur. Ut aliquam mollis varius. Quisque suscipit aliquet augue malesuada lacinia. Vivamus non quam lectus. Duis eu purus eget mi egestas vulputate id sit amet mauris. Donec ut enim sed lorem ultricies egestas. Nam feugiat ipsum eu nunc gravida, nec luctus ipsum viverra. Pellentesque at massa non nulla consectetur eleifend vitae a ante. Suspendisse aliquam condimentum eros, et pulvinar tellus maximus a. Aenean euismod accumsan dolor commodo luctus. Praesent in nibh nunc. In dictum ante ut massa fringilla, sed hendrerit lorem consequat.";
     let version = Version::Normal(1);
     let ec_level = ECLevel::L;
     let mask_pattern = MaskPattern::new(1);
 
     let qrb = QRBuilder::new(data.as_bytes())
-        .version(version)
+        // .version(version)
         .ec_level(ec_level)
         .palette(Palette::Poly)
         .mask(mask_pattern)
         .build()
         .unwrap();
+    let version = qrb.version();
 
     let path = Path::new("assets/output.png");
     let image = qrb.render_color(10);
